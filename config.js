d75581761920789ac7a38f9e240f82462d15d62f
vpffl5854-alt <vpffl5854@gmail.com>
2026-05-04 21:48:21 +0900

    Add runtime config

---
+++ config.js
@@ -0,0 +1,7 @@
+window.SNS_MAP_CONFIG = {
+  kakaoJavaScriptKey: "6183839db0fe5e3d8b87c5868389d084",
+  supabaseUrl: "https://tuerxtzydpmkvmacksgt.supabase.co",
+  supabaseAnonKey: "sb_publishable_BTrXsrW6zDkteyfJPFS73A_Fr-vjaTG",
+  supabaseTable: "spots",
+  linkPreviewEndpoint: "/api/link-preview"
+};
